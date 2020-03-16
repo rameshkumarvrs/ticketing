54721ab5110c6975380c61a5da627a2cc1192ee0
rameshkumarvrs@gmail.com <rameshkumarvrs@gmail.com>
2020-03-16 18:52:17 +0530

    added events in the home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_115610) do
+ActiveRecord::Schema.define(version: 2020_03_16_123200) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
